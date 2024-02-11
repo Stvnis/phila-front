edb2e6498c21c424b43a69cc38f9eb69285f443b
Vitalii <akumuliation@gmail.com>
2024-02-11 21:05:05 +0200

    add linter

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,41 +1,41 @@
-'use client'
-import { createTheme } from "@mui/material/styles";
+'use client';
+import { createTheme } from '@mui/material/styles';
 import { EUkraine } from '@phila-front/assets/fonts';
 
 
 export const theme = createTheme({
-    palette: {
-        mode: 'light',
-        primary: {
-          main: '#FFDC61',
-          dark: '#ffedae',
-          light: '#ffdb65',
-        },
-        secondary: {
-          main: '#262626',
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#FFDC61',
+      dark: '#ffedae',
+      light: '#ffdb65',
+    },
+    secondary: {
+      main: '#262626',
+    },
+  },
+  typography: {
+    fontFamily: [
+      EUkraine.style.fontFamily,
+      'sans-serif',
+    ].join(','),
+
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+          // border: 0,
+          // borderRadius: 3,
+          // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+          boxShadow: 'none',
+          // color: 'white',
+          // height: 48,
+          // padding: '0 30px',
         },
       },
-    typography: {
-            fontFamily: [
-                EUkraine.style.fontFamily,
-                'sans-serif',
-            ].join(','),
-
     },
-    components: {
-        MuiButton: {
-            styleOverrides: {
-              root: {
-                // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-                // border: 0,
-                // borderRadius: 3,
-                // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-                boxShadow: "none",
-                // color: 'white',
-                // height: 48,
-                // padding: '0 30px',
-              },
-            },
-          },
-    }
+  },
 }); 
