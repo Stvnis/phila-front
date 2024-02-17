5ae96eace9e062ebb95fa5508db20d3fb6e338ba
Vitalii <akumuliation@gmail.com>
2024-02-17 20:21:24 +0200

    add some fixes

--- src/sections/Donate/Donate.tsx
+++ src/sections/Donate/Donate.tsx
@@ -1,20 +1,23 @@
-import { Button, Container, Grid, Typography } from "@mui/material";
-import Image from "next/image";
+import { Button, Container, Grid, Typography } from '@mui/material';
+import Image from 'next/image';
 
-export const Donate=()=>{
-    return (
-        <Container>
-            <Grid container>
-                <Grid item xs={6}>
-                    <Typography>НАБЛИЖАЙ ПЕРЕМОГУ</Typography>
-                    <Typography>Волонтерство та допомога</Typography>
-                    <Typography>Почни змінювати себе в кращу сторону разом з християнською церквою “Філадельфія” змінювати себе в кращу сторону разом з християнською церквою “Філадельфія” змінювати себе в кращу сторону разом з християнською церквою “Філадельфія” Приєднуйся до нас та наближай перемогу!</Typography>
-                    <Button variant="contained">Допомогти</Button>
-                </Grid>
-                <Grid item xs={6}>
-                    <Image width="500" height="500" alt="preview" src="https://www.reconnectwithnature.org/getmedia/bbf87a4f-2bb7-48ca-b948-8ebff37dc835/Great-horned-owl-Shutterstock_1.jpg?width=1500&height=1084&ext=.jpg" />
-                </Grid>
-            </Grid>
-        </Container>
-    );
-}
+export const Donate = () => {
+  return (
+    <Container>
+      <Grid container>
+        <Grid item xs={6}>
+          <Typography>НАБЛИЖАЙ ПЕРЕМОГУ</Typography>
+          <Typography>Волонтерство та допомога</Typography>
+          <Typography>Почни змінювати себе в кращу сторону разом з християнською церквою “Філадельфія” змінювати себе в
+            кращу сторону разом з християнською церквою “Філадельфія” змінювати себе в кращу сторону разом з
+            християнською церквою “Філадельфія” Приєднуйся до нас та наближай перемогу!</Typography>
+          <Button variant="contained">Допомогти</Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Image width="500" height="500" alt="preview"
+                 src="https://www.reconnectwithnature.org/getmedia/bbf87a4f-2bb7-48ca-b948-8ebff37dc835/Great-horned-owl-Shutterstock_1.jpg?width=1500&height=1084&ext=.jpg" />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
