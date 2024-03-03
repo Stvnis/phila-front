a5526da90a005356fe330cf6702e93475cb6d891
Vitalii <akumuliation@gmail.com>
2024-03-03 22:55:51 +0200

    fix yarn error

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
     coverage: {
-      provider: 'istanbul' // or 'v8'
+      provider: 'istanbul', // or 'v8'
     },
   },
-})
+  resolve: {
+    alias: {
+      '@phila-front': path.resolve(__dirname, './src'),
+    },
+  },
+});
